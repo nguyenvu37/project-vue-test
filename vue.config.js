2e2bdfa8428588564d7ba76b3c311438187c96de
nguyenvu37 <nguyenanhvu@enjoyworks.co.kr>
2022-09-22 09:25:01 +0700

    finish project

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production"
-      ? "https://github.com/nguyenvu37/project-vue-test"
-      : "/",
-});
+    process.env.NODE_ENV === 'production' ? '/project-vue-test/' : '/',
+};
